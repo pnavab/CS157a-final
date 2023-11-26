862806a1956526c5585d9a3a5ff14f961171f8b7
pnavab <114110926+pnavab@users.noreply.github.com>
2023-11-25 22:14:44 -0800

    placeholder

--- app/signup/page.js
+++ app/signup/page.js
@@ -63,7 +63,7 @@ export default function Home() {
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullname">
                   <p> Full Name </p>
                 </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fullname" name="fullname" onChange={handleInputChange} type="text" placeholder="Johnny Sins"/>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fullname" name="fullname" onChange={handleInputChange} type="text" placeholder="John Smith"/>
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">

b26de94844de41565712cc3763df3b00209031ba
pnavab <114110926+pnavab@users.noreply.github.com>
2023-12-03 01:10:42 -0800

    testing

--- tables.sql
+++ tables.sql
@@ -61,3 +61,19 @@ CREATE VIEW most_active_users AS SELECT
         user.id
     ORDER BY
         post_count DESC;
+
+INSERT INTO user (username, fullname, password, role) VALUES ('admin', 'Admin', 'admin', 'admin')
+INSERT INTO user (username, fullname, password, role) VALUES ('john_doe', 'John Doe', 'securepassword', 'user');
+INSERT INTO user (username, fullname, password, role) VALUES ('bob1', 'Bob Robert', 'pw123', 'user');
+INSERT INTO post (title, description, user_id) VALUES ('Introduction to SQL', 'Exploring the basics of SQL', 2);
+INSERT INTO post (title, description, user_id) VALUES ('Hi Everyone', 'Class Grove is awesome', 2);
+INSERT INTO comment (description, user_id, post_id) VALUES ('Great article!', 2, 1);
+INSERT INTO comment (description, user_id, post_id) VALUES ('Hello!', 3, 2);
+
+UPDATE user SET fullname = 'Jane Doe' WHERE username = 'john_doe';
+UPDATE post SET description = 'Understanding SQL queries' WHERE id = 1;
+
+DELETE FROM user WHERE username = 'john_doe';
+DELETE FROM post WHERE id = 1;
+DELETE FROM comment WHERE id = 1;
+
